b45a51b483bc48873975e1928bdc1e1d2558c1c0
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-05-29 20:22:29 +0530

    Functionality added to Contact Section through emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,10 @@ import {EarthCanvas} from "./canvas";
 import {SectionWrapper} from "../hoc";
 import { slideIn } from "../utils/motion";
 
+// 
+// 
+// 
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -16,9 +20,43 @@ const Contact = () => {
   });
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    const { name, value} = e.target;
 
-  const handleSubmit = (e) => {};
+    setForm({ ...form, [name] : value})
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs.send(
+    'service_256fyjo', 
+    'template_pb2xyp9',
+    {
+      from_name: form.name,
+      to_name: 'Sandeep Kumar',
+      from_email: form.email,
+      to_email: 'lvssandeepkumar12@gmail.com',
+      message: form.message
+    },
+    'qXQUsND_rvkozgk3l'
+    ).then(() => {
+      setLoading(false);
+      alert('Thank You. I will get back to you as soon as possible.');
+
+      setForm({
+        name: "",
+        email: '',
+        message: ''
+      }, (error) => {
+        setLoading(false);
+        console.log(error);
+
+        alert('Something went wrong.')
+      })
+    })
+  };
 
   return (
     <div className="xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden">
